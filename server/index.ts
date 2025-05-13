e984fc4ba120381376cdc1678c0e19b860ea66e4
Caml2001 <camlwh013241@gmail.com>
2025-05-12 18:09:49 -0600

    vercel

--- server/index.ts
+++ server/index.ts
@@ -56,10 +56,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 5001;
+  // Use port from environment variable for Vercel compatibility
+  const port = process.env.PORT || 5001;
   server.listen({
     port,
     host: "0.0.0.0"
